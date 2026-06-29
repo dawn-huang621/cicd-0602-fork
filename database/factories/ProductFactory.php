b3d7e719d438672d851bb31ce986332092f9cd61
zt <q2379685@gmail.com>
2026-06-29 09:41:18 +0800

    [fix]:修改產品生成假資料改成更貼近現實產品

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -16,11 +16,34 @@ class ProductFactory extends Factory
      */
     public function definition(): array
     {
+        $products = [
+            ['name' => '筆記型電腦', 'price' => 28900],
+            ['name' => '桌上型電腦', 'price' => 35900],
+            ['name' => '24吋液晶螢幕', 'price' => 5990],
+            ['name' => '無線滑鼠', 'price' => 790],
+            ['name' => '機械式鍵盤', 'price' => 2490],
+            ['name' => '雷射印表機', 'price' => 8990],
+            ['name' => '多功能事務機', 'price' => 12900],
+            ['name' => '網路交換器 24 Port', 'price' => 6500],
+            ['name' => '無線基地台', 'price' => 3200],
+            ['name' => 'NAS 儲存設備', 'price' => 25900],
+            ['name' => '辦公椅', 'price' => 4500],
+            ['name' => '辦公桌', 'price' => 6800],
+            ['name' => 'A4 影印紙', 'price' => 150],
+            ['name' => '碳粉匣', 'price' => 3200],
+            ['name' => '條碼掃描器', 'price' => 2900],
+            ['name' => '工業用平板電腦', 'price' => 18500],
+            ['name' => 'PLC 控制器', 'price' => 42000],
+            ['name' => '變頻器', 'price' => 9800],
+            ['name' => '感測器模組', 'price' => 1200],
+            ['name' => '工業電源供應器', 'price' => 3500],
+        ];
+
+        $product = fake()->randomElement($products);
         return [
-            'name' => $this->faker->word(),
-            'price' => $this->faker->numberBetween(300, 1000),
-            'description' => $this->faker->sentence(),
-            'amount' => $this->faker->numberBetween(1, 20),
+            'name' => $product['name'],
+            'price' => $product['price'],
+            'description' => fake('zh_TW')->sentence(),
         ];
     }
 }
